5dd5b8caa18342ccb16ffb0189556b3f5a09774e
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-11-14 15:35:51 +0300

    doc: doxygen comments updated

--- include/str.h
+++ include/str.h
@@ -6,7 +6,10 @@
  * 
  * @brief   String library API
  * 
- * API header for the C language string library 
+ * API header for the String library.
+ * 
+ * Library provides dynamic heap-based string type and set of functions
+ * implementing basic string operations.
  * 
  *****************************************************************************/
 
@@ -21,58 +24,71 @@
 typedef struct str_t str_t;
 
 /**
- * @brief Creates new instance of the string holding contents
- *        of the provided null-terminated byte string.
+ * @brief Creates new instance of string.
+ * 
+ * Constructs new heap-allocated string instance based
+ * on the provided null-terminated ASCII string.
+ * Constructed string instance contains only valid ASCII
+ * characters. Non-ASCII characters are replaced with '?' character. 
  * 
  * @param string Null-terminated byte string of valid ASCII characters
  * @return Pointer to the allocated instance of the string
  * @note If string is @c NULL an empty string with default capacity is created
+ * @note Non-ASCII characters will be replaced with the '?' character
+ * 
+ * @example['6'] str_example.c
  */
 str_t* str_new(const char* string);
 
 /**
- * @brief Created an empty string with the buffer of the given capacity.
+ * @brief Creates an empty string with the buffer of the given capacity.
  * 
  * @param capacity Capacity of the buffer
  * @return Pointer to the allocated instance of the string
- * @note If capacity is 0 no memory allocation is performed
- *       and buffer is empty
+ * @note If capacity is set to 0 no memory allocation
+ *      is performed, string buffer is empty
  */
 str_t* str_with_capacity(const size_t capacity);
 
 /**
- * @brief Creates the new copy of the string.
+ * @brief Creates copy of the string.
  * 
- * @param other String to be copied
- * @return Pointer to the new instance of @c str_t type which is
- *         a copy of the given string. If the given pointer is @c NULL
- *         returns an empty string.
+ * @param other String instance to be copied
+ * @return Pointer to the new instance of string which is
+ *      a copy of the given string;
+ *      If given pointer is @c NULL returns an empty string
+ * 
+ * @example['15'] str_example.c
  */
 str_t* str_copy(const str_t* other);
 
 /**
- * @brief Drops the buffer of the string and sets all attributes to 0.
+ * @brief Drops the string instance.
  * 
- * @param self Pointer to the initialised string instance
+ * Frees the allocated memory and sets string
+ * instance pointer to @c NULL  
+ * 
+ * @param self Pointer to pointer to the initialised string instance
  * @return void
- * @note If @code{self} is @c NULL function does nothing
+ * @note If @c self is @c NULL or if @c *self is @c NULL function does nothing
  * @warning After string is dropped it must not be used
+ * 
+ * @example['32'] str_example.c
  */
 void str_drop(str_t** self);
 
 /**
  * @brief Returns the length of the string.
  * 
- * @return Length of the string. If @c self is @c NULL ,
- *         0 is returned.
+ * @return Length of the string. If @c self is @c NULL , 0 is returned.
  */
 size_t str_len(const str_t* self);
 
 /**
  * @brief Returns the capacity of the string buffer.
  * 
- * @return Capacity of the string's buffer. If @c self is @c NULL
- *         0 is returned.
+ * @return Capacity of the string's buffer;
+ *      If @c self is @c NULL 0 is returned
  */
 size_t str_cap(const str_t* self);
 
@@ -81,7 +97,7 @@ size_t str_cap(const str_t* self);
  * 
  * @param self Pointer to the initialised string instance
  * @return @c true if @c self is empty;
- *         @c false otherwise or if the string @c NULL
+ *      @c false otherwise or if the string @c NULL
  */
 bool str_is_empty(const str_t* self);
 
@@ -91,21 +107,25 @@ bool str_is_empty(const str_t* self);
  * 
  * @param self Pointer to the initialised string instance
  * @param pos Position of the character in the given string
- * @return Character of the string on the given position; @c 0 if @c pos
- * is greater or equal to the length of the string or if the string is
- * empty or @c NULL
+ * @return Character of the string on the given position;
+ *      @c 0 if @c pos is greater or equal to the length
+ *      of the string or if the string is empty or @c NULL
+ * 
+ * @example['41'] str_example.c
  */
 char str_at(const str_t* self, const size_t pos);
 
 /**
  * @brief Returns a pointer to the string buffer.
  * 
- * String buffer is holding valid Null-terminated ASCII string literal,
+ * String buffer is holding valid null-terminated ASCII string literal,
  * so pointer to the string buffer may be used as
  * normal @c const @c char* string literal.
  * 
  * @param self Pointer to the string instance
- * @return Pointer to the @c self buffer; @c NULL if @c self is @c NULL 
+ * @return Pointer to the @c self buffer; @c NULL if @c self is @c NULL
+ * 
+ * @example['51'] str_example.c
  */
 const char* str_as_ptr(const str_t* self);
 
@@ -115,10 +135,12 @@ const char* str_as_ptr(const str_t* self);
  * Function changes the first string by adding all characters of
  * the second string to it. Second string stays unchanged.
  * 
- * @param self Pointer to the @c str_t instance, which will be altered
+ * @param self Pointer to the string instance, which will be altered
  * @param string Null-terminated string literal with valid ASCII characters
- * @return Pointer to the @c self string
- * @note Function does nothing to the @c self if @c string is @c NULL or empty
+ * @return Pointer to the @c self string instance
+ * @note Function does nothing to @c self if @c string is @c NULL or empty
+ * 
+ * @example['61'] str_example.c
  */
 str_t* str_append(str_t* self, const char* string);
 
@@ -126,9 +148,11 @@ str_t* str_append(str_t* self, const char* string);
  * @brief Creates new string which is a result of concatenation of
  * the given strings.
  * 
- * @param str_a,str_b Pointesr to the @c str_t instances
- * @return Pointer to the new @c str_t instance - a result
- *         of concatenation of the given strings
+ * @param str_a,str_b Pointers to the string instances
+ * @return Pointer to the new string instance - a result
+ *      of concatenation of the given strings
+ * 
+ * @example['81'] str_example.c
  */
 str_t* str_concat(const str_t* str_a, const str_t* str_b);
 
@@ -136,10 +160,12 @@ str_t* str_concat(const str_t* str_a, const str_t* str_b);
  * @brief Trims leading and trailing whitespace characters in the string.
  * 
  * Trims leading and trailing @b space, @b h-tab, @b v-tab, @b newline
- * symbols if any exitsts. Function has linear complexity.
+ * symbols if any exitsts. Function has linear complexity @itO(n).
  * 
  * @param self Pointesr to the @c str_t instances
  * @returns Pointer to the altered @c self string
+ * 
+ * @example['94'] str_example.c
  */
 str_t* str_trim(str_t* self);
 
