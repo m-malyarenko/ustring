3df4f37dfe1a5e952a1299b2812dd9d34a811fc7
Mikhail Malyarenko <malyarenko.md@gmail.com>
2023-01-07 20:01:15 +0300

    test: add tests for str

--- test/str_test.c
+++ test/str_test.c
@@ -1,6 +1,6 @@
 #include <criterion/criterion.h>
 
-#include "../include/str.h"
+#include <ustring/str.h>
 #include "../src/str_p.h"
 
 static str_t* string_a;
@@ -48,9 +48,9 @@ Test(str, with_capacity) {
     cr_assert_not_null(string->buffer);
 
     string = str_with_capacity(0);
-    cr_assert_eq(string->cap, 0);
+    cr_assert_eq(string->cap, 1);
     cr_assert_eq(string->len, 0);
-    cr_assert_null(string->buffer);
+    cr_assert_str_eq(string->buffer, "");
 
     str_drop(&string);
 }
@@ -84,261 +84,142 @@ Test(str, is_empty) {
 }
 
 Test(str, at) {
-    str_t* expr_a = str_new("int x = 0 \ny = 0 \n( x != 0 ) || ( y != 0 ) \n");
-    str_t* expr_b = str_new("y = y * 78 \ny > 90 \n");
-
-    str_replace(expr_a, ">", "\\>");
-    str_replace(expr_b, ">", "\\>");
-
-    // str_replace(expr_a, "|", "\\|");
-    str_replace(expr_b, "|", "\\|");
-
-    cr_assert_str_eq(expr_a->buffer, "int x = 0 \ny = 0 \n( x != 0 ) \\|\\| ( y != 0 ) \n");
-    cr_assert_str_eq(expr_b->buffer, "y = y * 78 \ny \\> 90 \n");
-
-    // str_drop(&expr_a);
-    str_drop(&expr_b);
+    cr_assert_eq(str_at(string_a, 0), 'P');
+    cr_assert_eq(str_at(string_a, 5), '&');
+    cr_assert_eq(str_at(string_a, 10), 'r');
+    cr_assert_eq(str_at(string_a, 11), 0);
+    cr_assert_eq(str_at(string_a, 100), 0);
+    cr_assert_eq(str_at(NULL, 0), 0);
 }
 
 Test(str, append) {
+    str_append(string_a, " & Break");
+    cr_assert_str_eq(string_a->buffer, "Pull & Bear & Break");
+    str_append(string_a, NULL);
+    cr_assert_str_eq(string_a->buffer, "Pull & Bear & Break");
+    str_append(string_a, "");
+    cr_assert_str_eq(string_a->buffer, "Pull & Bear & Break");
+    str_append(string_a, str_as_ptr(string_a));
+    cr_assert_str_eq(string_a->buffer, "Pull & Bear & BreakPull & Bear & Break");
+}
 
+Test(str, clear) {
+    const size_t old_cap = str_cap(string_a);
+    str_clear(string_a);
+    const size_t new_cap = str_cap(string_a);
+    cr_assert_eq(string_a->len, 0);
+    cr_assert_eq(old_cap, new_cap);
 }
 
 Test(str, concat) {
+    str_t* string_a_b = str_concat(string_a, string_b);
+    cr_assert_str_eq(string_a_b->buffer, "Pull & BearOne Two Three");
+    str_t* string_a_a = str_concat(string_a, string_a);
+    cr_assert_str_eq(string_a_a->buffer, "Pull & BearPull & Bear");
+    str_t* string_a_b_c = str_concat(string_a_b, string_c);
+    cr_assert_str_eq(string_a_b_c->buffer, "Pull & BearOne Two ThreeAbCd");
+    str_t* string_b_null = str_concat(string_b, NULL);
+    cr_assert_str_eq(string_b_null->buffer, "One Two Three");
+    str_t* string_null_b = str_concat(NULL, string_b);
+    cr_assert_str_eq(string_null_b->buffer, "One Two Three");
+    str_t* string_null_null = str_concat(NULL, NULL);
+    cr_assert_str_eq(string_null_null->buffer, "");
+    cr_assert_eq(str_len(string_null_null), 0);
 
+    str_drop(&string_a_b);
+    str_drop(&string_a_a);
+    str_drop(&string_a_b_c);
+    str_drop(&string_b_null);
+    str_drop(&string_null_b);
+    str_drop(&string_null_null);
 }
 
 Test(str, trim) {
+    str_t* string_white = str_new("  \t\n \vHello  \t\n\r World");
+    str_trim(string_white);
+    cr_assert_str_eq(string_white->buffer, "Hello  \t\n\r World");
 
+    str_clear(string_white);
+    str_append(string_white, "Hello  \t\n\r World  \t\n \v\n\r");
+    str_trim(string_white);
+    cr_assert_str_eq(string_white->buffer, "Hello  \t\n\r World");
+
+    str_clear(string_white);
+    str_append(string_white, "  \t\n \vHello  \t\n\r World  \t\n \v\n\r");
+    str_trim(string_white);
+    cr_assert_str_eq(string_white->buffer, "Hello  \t\n\r World");
+
+    str_drop(&string_white);
 }
 
-Test(str, replace) {
-
+Test(str, eq) {
+    cr_assert(str_eq(string_a, string_a));
+    cr_assert_not(str_eq(string_b, string_a));
+    cr_assert_not(str_eq(string_b, NULL));
+    cr_assert_not(str_eq(NULL, NULL));
+    cr_assert_not(str_eq(NULL, string_b));
 }
 
-Test(str, literal_len) {
+// Test(str, truncate) {
 
-}
-
-Test(str, literal_contains) {
-
-}
-
-// static void str_is_empty_test() {
-//     str_t* str1 = str_new(NULL);
-//     str_t* str2 = NULL;
-//     str_t* str3 = str_new("");
-//     str_t* str4 = str_with_capacity(0);
-//     str_t* str5 = str_with_capacity(5);
-//     str_t* str6 = str_new("ABCD");
-
-//     assert(str_is_empty(str1));
-//     assert(str_is_empty(str2));
-//     assert(str_is_empty(str3));
-//     assert(str_is_empty(str4));
-//     assert(str_is_empty(str5));
-//     assert(!str_is_empty(str6));
-
-//     str_drop(str1);
-//     str_drop(str2);
-//     str_drop(str3);
-//     str_drop(str4);
-//     str_drop(str5);
-//     str_drop(str6);
 // }
 
-// static void str_at_test() {
-//     const char* str_lit = "abcdef";
-//     str_t* str1 = str_new(str_lit);
-//     str_t* str2 = str_new("");
+// Test(str, contains) {
 
-//     for (size_t i = 0; i < str_len(str1); i++) {
-//         assert(str_at(str1, i) == str_lit[i]);
-//     }
-
-//     assert(str_at(str1, 23) == 0);
-//     assert(str_at(NULL, 0) == 0);
-//     assert(str_at(str2, 0) == 0);
-//     assert(str_at(str2, 23) == 0);
-
-//     str_drop(str1);
-//     str_drop(str2);
 // }
 
-// static void str_append_test() {
-//     str_t* str_a = str_new("One Two Three");
-//     size_t old_len_a = str_len(str_a);
-//     str_t* str_b = str_new(" Four");
-//     size_t old_len_b = str_len(str_b);
-//     str_t* str_c = str_with_capacity(0);
+// Test(str, contains_fn) {
 
-//     assert(str_append(NULL, str_as_ptr(str_b)) == NULL);
-//     assert(str_append(NULL, NULL) == NULL);
-//     str_t* str_a_copy = str_copy(str_a);
-//     assert(str_append(str_a, NULL) != NULL);
-//     assert(str_len(str_a) == old_len_a);
-//     assert(strcmp(str_as_ptr(str_a), str_as_ptr(str_a_copy)) == 0);
-//     str_drop(str_a_copy);
-
-//     assert(str_append(str_a, str_as_ptr(str_b)) != NULL);
-//     assert(strcmp(str_as_ptr(str_a), "One Two Three Four") == 0);
-//     assert(strcmp(str_as_ptr(str_b), " Four") == 0);
-//     assert(str_len(str_a) == (old_len_a + old_len_b));
-
-//     assert(str_append(str_a, str_as_ptr(str_c)) != NULL);
-//     assert(strcmp(str_as_ptr(str_a), "One Two Three Four") == 0);
-
-//     assert(str_append(str_a, " Five") != NULL);
-//     assert(strcmp(str_as_ptr(str_a), "One Two Three Four Five") == 0);
-
-//     str_drop(str_a);
-//     str_drop(str_b);
-//     str_drop(str_c);
 // }
 
-// static void str_concat_test() {
-//     str_t* string = str_concat(NULL, NULL);
-//     assert(string != NULL);
-//     assert(string->len == 0);
-//     assert(string->buffer != 0);
-//     str_drop(string);
+// Test(str, trim_matches) {
 
-//     string = str_concat(NULL, str_with_capacity(5));
-//     assert(string != NULL);
-//     assert(string->len == 0);
-//     assert(string->buffer != 0);
-//     str_drop(string);
-
-//     string = str_concat(str_with_capacity(5), NULL);
-//     assert(string != NULL);
-//     assert(string->len == 0);
-//     assert(string->buffer != 0);
-//     str_drop(string);
-
-//     string = str_concat(str_with_capacity(5), str_with_capacity(5));
-//     assert(string != NULL);
-//     assert(string->len == 0);
-//     assert(string->buffer != 0);
-//     str_drop(string);
-
-//     str_t* str_a = str_new("One Two Three");
-//     size_t old_len_a = str_len(str_a);
-//     str_t* str_b = str_new(" Four");
-//     size_t old_len_b = str_len(str_b);
-//     str_t* concat_str = str_concat(str_a, str_b);
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == (str_len(str_a) + str_len(str_b)));
-//     assert(strcmp(str_as_ptr(concat_str), "One Two Three Four") == 0);
-//     str_drop(concat_str);
-
-//     concat_str = str_concat(str_b, str_a);
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == (str_len(str_a) + str_len(str_b)));
-//     assert(strcmp(str_as_ptr(concat_str), " FourOne Two Three") == 0);
-//     str_drop(concat_str);
-
-//     concat_str = str_concat(str_a, NULL);
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == str_len(str_a));
-//     assert(strcmp(str_as_ptr(concat_str), "One Two Three") == 0);
-//     str_drop(concat_str);
-
-//     concat_str = str_concat(str_a, str_with_capacity(5));
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == str_len(str_a));
-//     assert(strcmp(str_as_ptr(concat_str), "One Two Three") == 0);
-
-//     concat_str = str_concat(NULL, str_a);
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == str_len(str_a));
-//     assert(strcmp(str_as_ptr(concat_str), "One Two Three") == 0);
-//     str_drop(concat_str);
-
-//     concat_str = str_concat(str_with_capacity(5), str_a);
-//     assert(concat_str != NULL);
-//     assert((old_len_a == str_a->len) && (old_len_b == str_b->len));
-//     assert(str_len(concat_str) == str_len(str_a));
-//     assert(strcmp(str_as_ptr(concat_str), "One Two Three") == 0);
-//     str_drop(concat_str);
-
-//     str_drop(str_a);
-//     str_drop(str_b);
 // }
 
-// static void str_trim_test() {
-//     str_t* str1 = str_new("");
-//     str_t* str2 = str_new("  \t\n\vabc");
-//     str_t* str3 = str_new("  \t\nabc \t\v  \n\r");
-//     str_t* str4 = str_new("abc abc\t abc\n\t   ");
-//     str_t* str5 = str_new("  \t\n\t  \v\r   ");
+// Test(str, trim_matches_fn) {
 
-//     str_trim(str1);
-//     assert(strcmp(str_as_ptr(str1), "") == 0);
-//     assert(str_is_empty(str1));
-
-//     str_trim(str2);
-//     assert(strcmp(str_as_ptr(str2), "abc") == 0);
-
-//     str_trim(str3);
-//     assert(strcmp(str_as_ptr(str3), "abc") == 0);
-
-//     str_trim(str4);
-//     assert(strcmp(str_as_ptr(str4), "abc abc\t abc") == 0);
-
-//     str_trim(str5);
-//     assert(strcmp(str_as_ptr(str5), "") == 0);
-//     assert(str_is_empty(str5));
-
-//     str_drop(str1);
-//     str_drop(str2);
-//     str_drop(str3);
-//     str_drop(str4);
-//     str_drop(str5);
 // }
 
-// static void _str_literal_len_test() {
-//     const char* str1 = NULL;
-//     const char* str2 = "";
-//     const char* str3 = "Hello";
+// Test(str, trim_start_matches) {
 
-//     assert(__str_literal_len(str1) == 0);
-//     assert(__str_literal_len(str2) == 0);
-//     assert(__str_literal_len(str3) == 5);
 // }
 
-// static void _str_literal_contains_test() {
-//     const char* str1 = NULL;
-//     const char* str2 = "";
-//     const char* str3 = "Hello";
-//     const char* str4 = "qwerty;qwerty?";
+// Test(str, trim_start_matches_fn) {
 
-//     assert(!__str_literal_contains(str1, 'a'));
-//     assert(!__str_literal_contains(str2, '\0'));
-//     assert(__str_literal_contains(str3, 'e'));
-//     assert(__str_literal_contains(str4, ';'));
-//     assert(__str_literal_contains(str4, '?'));
-//     assert(!__str_literal_contains(str4, 'z'));
 // }
 
-// int main() {
-//     str_new_test();
-//     str_with_capacity_test();
-//     str_copy_test();
-//     str_drop_test();
-//     str_len_test();
-//     str_cap_test();
-//     str_is_empty_test();
-//     str_at_test();
-//     str_append_test();
-//     str_concat_test();
-//     str_trim_test();
-//     _str_literal_len_test();
-//     _str_literal_contains_test();
+// Test(str, trim_end_matches) {
 
-//     return 0;
+// }
+
+// Test(str, trim_end_matches_fn) {
+
+// }
+
+// Test(str, replace) {
+
+// }
+
+// Test(str, starts_with) {
+
+// }
+
+// Test(str, ends_with) {
+
+// }
+
+// Test(str, to_lowercase) {
+
+// }
+
+// Test(str, to_uppercase) {
+
+// }
+
+// Test(str, literal_len) {
+
+// }
+
+// Test(str, literal_contains) {
+
 // }
